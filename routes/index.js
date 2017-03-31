361977ea8f2188010ae0f65451bf1e0906653c00
Simon Greensmith <simon.greensmith@digital.cabinet-office.gov.uk>
2017-03-31 17:28:55 +0100

    tidied up code in endpoints

--- routes/index.js
+++ routes/index.js
@@ -17,36 +17,25 @@ router.get('/', function(req, res, next) {
 router.post('/matching-service', function(req, res) {
 
   //console.log('/matching-service hit');
-  //console.dir(surnameData[0].value);
-  //console.dir(cycle3Dataset);
-
-  var surnameData = req.body.matchingDataset.surnames;
-  var cycle3Dataset = req.body.cycle3Dataset;
 
   res.set(jsonContent);
 
-  if (surnameData[0].value == 'Griffin')
+  if (req.body.matchingDataset.surnames[0].value == 'Griffin')
     res.send(match);
   else
-    if (cycle3Dataset && cycle3Dataset.attributes.nino == 'goodvalue')
-      res.send(match);
-    else
-      res.send(noMatch);
+    (req.body.cycle3Dataset && req.body.cycle3Dataset.attributes.nino == 'goodvalue') ? res.send(match) : res.send(noMatch);
 });
 
 router.post('/account-creation', function(req,res){
 
   //console.log("account creation hit");
-  //console.dir(req.body);
 
   var levelOfAssurance = req.body.levelOfAssurance;
 
   res.set(jsonContent);
 
-  if (levelOfAssurance == 'LEVEL_2')
-    res.send(success);
-  else
-    res.send(failure);
+  (levelOfAssurance == 'LEVEL_2') ? res.send(success) : res.send(failure);
+
 });
 
 module.exports = router;
